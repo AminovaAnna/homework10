61e091cc2846a3521c68b8939b9c30d6400332ed
AminovaAnna <aminovaanna163@gmail.com>
2023-11-02 21:14:52 +0300

    Дз выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
+        String fullName = lastName + ' ' + firstName + ' ' + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName); //задача1
+        System.out.println( );
+        String fullName1 = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName1); //задача2
+        System.out.println( );
+        String fullName2 = fullName.replace('ё', 'е');
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName2); //задача3
+
+
     }
 }
